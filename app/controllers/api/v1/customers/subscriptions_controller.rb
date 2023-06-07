1041c8c0b4c9a2eae4887f580063b412072885c8
Matt Enyeart <menyeart1@gmail.com>
2023-06-07 11:30:41 -0600

    feat:add subscription update method

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::Customers::SubscriptionsController < ApplicationController
+
   def create
     subscription = Subscription.new(subscription_params)
     if subscription.save
       render json: SubscriptionSerializer.new(subscription), status: :created
+    else
+      render json: ErrorSerializer.new(subscription.errors), status: :bad_request
+    end
+  end
+
+  def update
+    subscription = Subscription.find(params[:id])
+    if subscription.update(subscription_params)
+      render json: SubscriptionSerializer.new(subscription), status: :accepted
     else
       render json: ErrorSerializer.new(subscription.errors), status: :bad_request
     end
