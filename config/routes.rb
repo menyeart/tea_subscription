3f9f7677afe001bfcb55109ca11d59a7856aceef
Matt Enyeart <menyeart1@gmail.com>
2023-06-07 11:31:40 -0600

    feat:add subscription patch route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [] do
-        resources :subscriptions, only: [:create], controller: 'customers/subscriptions'
+        resources :subscriptions, only: [:create, :update], controller: 'customers/subscriptions'
       end
     end
   end
