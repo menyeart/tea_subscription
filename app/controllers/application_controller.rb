ac6a88de9c844c1bb7fcb208119e55a867d6264c
Matt Enyeart <menyeart1@gmail.com>
2023-06-07 11:31:03 -0600

    feat:add resuce and error handling methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_invalid_response
+  rescue_from StandardError, with: :render_invalid_request
+
+  def render_invalid_response(exception)
+    render json: ErrorSerializer.new(exception.message).invalid_request, status: :not_found
+  end
+
+  def render_invalid_request(exception)
+    render json: ErrorSerializer.new(exception.message).invalid_request, status: :bad_request
+  end
 end
