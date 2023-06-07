e0de7ad80fa7a82a2a1324c551499fcf8ce6cc62
Matt Enyeart <menyeart1@gmail.com>
2023-06-07 10:15:42 -0600

    fix:correct faker setup

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tea do
-    title { Faker::Tea }
+    title { Faker::Tea.variety }
     description { Faker::Books::Dune.quote }
     brew_time { [4.00, 4.30, 5.00].sample }
     temp { [150, 160, 180].sample }
