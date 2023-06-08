c61508ff82bd1f3ee9625578e7026a2e37eb32f2
Matt Enyeart <menyeart1@gmail.com>
2023-06-08 15:15:07 -0600

    feat: add additional subscription validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,7 @@ class Subscription < ApplicationRecord
   belongs_to :tea
 
   validates :title, :price, :status, :frequency, presence: true
+  validates :title, uniqueness: true
 
   enum status: { active: 0, cancelled: 1 }
   enum frequency: { weekly: 0, monthly: 1 }
